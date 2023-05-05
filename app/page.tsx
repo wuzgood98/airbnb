89230d991e3a6a25f5a94b66d83da976bbd9b0ab
Gideon  Addo <gideonaddoofori@gmail.com>
2023-05-05 18:14:36 +0000

    Change dynamic value to force dynamic rendering and dynamic data fetching

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ type HomeProps = {
   searchParams: IListingsParams;
 };
 
-export const dynamic = "auto";
+export const dynamic = "force-dynamic";
 
 export default async function Home({ searchParams }: HomeProps) {
   const listings = await getListings(searchParams);
